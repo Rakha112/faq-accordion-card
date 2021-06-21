eb159feb9a22a0a163bd58d8b8287a6d0997908a
Rakha <rakhawibowo1998@gmail.com>
2021-06-21 16:21:07 +0700

    initial commit

--- js/app.js
+++ js/app.js
@@ -2,24 +2,16 @@ const tombol = document.querySelectorAll(".header")
 const icon = document.querySelectorAll(".icon")
 const isi = document.querySelectorAll(".diaktif")
 
-// for (let i = 0; i < tombol.length; i++) {
-//     tombol[i].addEventListener("click", e => {
-//         isi[i].classList.toggle("diaktif")
-//         isi[i].classList.toggle("aktif")
-//         icon[i].classList.toggle("icon_aktif")
-//     })
-// }
-
-
-tombol.forEach(button => {
-  button.addEventListener('click',function(e) {
-    button.closest('.konten_isi_faq').classList.toggle('aktif');
-    tombol.forEach(one => {
-      let question = one.closest('.konten_isi_faq');
-      if(question != button.closest('.konten_isi_faq')) {
-        question.classList.remove('aktif');
-      }
+for (let i = 0; i < tombol.length; i++) {
+    tombol[i].addEventListener("click", e => {
+        tombol[i].closest('.konten_isi_faq').classList.toggle('aktif')
+        icon[i].classList.add("icon_aktif")
+        for(let j = 0; j < tombol.length; j++) {
+            let a = tombol[j].closest('.konten_isi_faq');
+            if(a != tombol[i].closest('.konten_isi_faq')) {
+              a.classList.remove('aktif');
+              icon[j].classList.remove("icon_aktif")
+            }
+        }
     })
-    
-  })
-})
+}
